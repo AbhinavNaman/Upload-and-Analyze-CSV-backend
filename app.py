a6ffa2b2e5894578a467f29c6ea1022d2146b179
Abhinav-Naman <abhinav@edcults.com>
2024-06-25 18:32:32 +0530

    render deploy process

--- app.py
+++ app.py
@@ -73,5 +73,8 @@ def proxy_get():
         logging.error(f"Error in proxy_get: {e}")
         return jsonify({'error': str(e)}), 500
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
